75e8a24bb5fb3c24fe5e5f3134697c7cfac80a49
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-08-20 10:18:01 -0600

    advanced queries complete

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -2,17 +2,24 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE authors.name='George R.R. Martin';
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE authors.name='Milan Kundera';
 
 -- Find all books written by an author from China or the UK.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE authors.nationality='China' OR authors.nationality='United Kingdom';
 
 -- Find out how many books Albert Camus wrote.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE authors.name='Albert Camus';
 
 -- Find out how many books written before 1980 were by authors not from the US.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE books.publication_date < 1980 AND authors.nationality <> 'United States of America';
 
 -- For these last two, you should not need a JOIN.
 
 -- Find all authors whose names start with 'J'.
+SELECT * FROM authors WHERE name LIKE 'J%';
 
 -- Find all books whose titles contain 'the'.
+SELECT * FROM books WHERE title LIKE '%the%';
