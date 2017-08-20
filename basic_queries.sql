2ddc7e18cb6a0fcd0f0973b849980e466563f442
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-08-20 10:14:55 -0600

    beginner section complete

--- basic_queries.sql
+++ basic_queries.sql
@@ -3,29 +3,57 @@
 
 -- Get all information about all authors
 
+SELECT * FROM authors;
+
 -- Get just the name and birth year of all authors
+
+SELECT name, birth_year FROM authors;
 
 -- Get all authors born in the 20th centruy or later
 
+SELECT name FROM authors WHERE birth_year > 1900;
+
 -- Get all authors born in the USA
+
+SELECT name FROM authors WHERE nationality = 'United States of America';
 
 -- Get all books published on 1985
 
+SELECT title FROM books WHERE publication_date = 1985;
+
 -- Get all books published before 1989
+
+SELECT title, publication_date FROM books WHERE publication_date < 1989;
 
 -- Get just the title of all books.
 
+SELECT title FROM books;
+
 -- Get just the year that 'A Dance with Dragons' was published
-  -- Cry when you realize how long it's been
+
+SELECT title, publication_date FROM books WHERE title = 'A Dance with Dragons';
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
 
+SELECT title FROM books WHERE title LIKE '%the%';
+
 -- Add yourself as an author
+
+INSERT INTO authors VALUES (9, 'Sam Ascher', 'United States of America', 1990);
 
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
 
+INSERT INTO books VALUES (1000, 'The Life of SB Ascher', 2020, '201');
+INSERT INTO books VALUES (1001, 'The Life of SB Ascher pt 2', 2020, '201');
+
 -- Update one of your books to have a new title
+
+UPDATE books SET title = 'SB Ascher: the Early Years' WHERE title = 'The Life of SB Ascher pt 2';
 
 -- Delete your books
 
+DELETE FROM books WHERE author_id = 201;
+
 -- Delete your author entry
+
+DELETE FROM authors where name = 'Sam Ascher';
